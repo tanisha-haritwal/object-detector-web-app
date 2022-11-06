66060748f458d6f8a7f567eb73376d9a306a085a
tanisha-haritwal <86518130+tanisha-haritwal@users.noreply.github.com>
2022-11-06 16:09:57 +0530

    Add files via upload

---
+++ chair.js
@@ -0,0 +1,27 @@
+function back(){
+    window.location="index.html";
+    }
+    status="";
+    function preload(){
+    img = loadImage("Chair.jpg");
+    }
+    function setup(){
+    canvas = createCanvas(600, 400);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Object";
+    }
+    function modelLoaded(){
+    console.log("Model Loaded!");
+    status=true;
+    objectDetector.detect(img, gotResults);
+    }
+    function gotResults(error, results){
+    if(error){
+    console.log(error);
+    }
+    console.log(results);
+    }
+    function draw(){
+    image(img, 0, 0, 600, 400);
+    }
